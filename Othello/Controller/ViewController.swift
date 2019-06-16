22cafe8c6be9c4cf545b96442aa4ce2f0d312105
Bartosz Bilski <bilskib@gmail.com>
2019-06-17 00:06:44 +0200

    black & white graphics added. minor changes to ViewController (before draw func).

--- Othello/Controller/ViewController.swift
+++ Othello/Controller/ViewController.swift
@@ -11,21 +11,70 @@ import SpriteKit
 
 class ViewController: UIViewController {
     
+    var board = Board()
+    var strategist: Strategist!
+    
+    let scene = GameScene(size: UIScreen.main.bounds.size)
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as? SKView {
-            let scene = GameScene(size: UIScreen.main.bounds.size)
+            //let scene = GameScene(size: UIScreen.main.bounds.size)
             scene.scaleMode = .aspectFill
             view.presentScene(scene)
         }
-    
-        let game = Othello()
-        game.clear()
-        game.setInitialBoard()
-        game.printBoardGraphicToConsole()
+        
+        setInitialBoard()
+        printBoardGraphicToConsole()
+        //updateViewFromModel()
     }
     
+    // MARK: Initial Set-up
+    func setInitialBoard() {
+        board[3,3] = .white
+        board[4,4] = .white
+        board[3,4] = .black
+        board[4,3] = .black
+    }
+    
+    
+    
+    
+    
+//    func updateViewFromModel() {
+//        let blackPieceNode = SKSpriteNode(imageNamed: "black")
+//        let whitePieceNode = SKSpriteNode(imageNamed: "white")
+//        for i in 0..<board.rows {
+//            for j in 0..<board.columns {
+//                if board[i,j] == .black {
+//                    scene.addChild(blackPieceNode)
+//                    scene.gamePieceNodes.append(blackPieceNode)
+//                } else if board[i,j] == .white {
+//                    scene.addChild(whitePieceNode)
+//                    scene.gamePieceNodes.append(whitePieceNode)
+//                }
+//            }
+//        }
+//    }
+    
+    // MARK: Helper methods
+    func printBoardGraphicToConsole() {
+        print("..................................................")
+        print("  0     1     2     3     4     5     6     7  /")
+        for i in 0..<board.columns {
+            print("-------------------------------------------------")
+            for j in 0..<board.rows {
+                print(board[i,j], terminator: "|")
+            }
+            print(i)
+        }
+        print("-------------------------------------------------")
+    }
+    
+    // MARK: Other properties
     override var shouldAutorotate: Bool {
         return false
     }
